6a49d4703b08df9c357754ee9678e98b3c19a7e5
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2022-10-25 12:43:32 -0700

    Visualize

--- automate.py
+++ automate.py
@@ -1,17 +1,19 @@
 import os
-import glob
-import shutil
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # run
 os.system("abaqus cae noGUI=py_model.py")
 
-# move generated files to a folder
-# os.makedirs("outputs", exist_ok=True)
-# for file in glob.iglob("abaqus.rp*", recursive=True):
-#     filename = os.path.basename(file)
-#     shutil.move(file, "outputs/" + filename)
-# for file in glob.iglob("exterior*", recursive=True):
-#     filename = os.path.basename(file)
-#     shutil.move(file, "outputs/" + filename)
-
+# Visualize
+nodes = pd.read_csv("nodes.csv")
+x, y, z = nodes.x, nodes.y, nodes.z
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(projection='3d')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+sca = ax.scatter(x, y, z, c=nodes.s11)
+plt.colorbar(sca, pad=0.1)
+plt.show()
